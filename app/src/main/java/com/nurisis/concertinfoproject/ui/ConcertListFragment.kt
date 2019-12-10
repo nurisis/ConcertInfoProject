4f20f2504d134d43fb497f6acdd382a56a5c68b1
nurisis <isoyeong666@naver.com>
2019-12-10 19:58:58 +0900

    Write a setting about R8 in gradle

--- app/src/main/java/com/nurisis/concertinfoproject/ui/ConcertListFragment.kt
+++ app/src/main/java/com/nurisis/concertinfoproject/ui/ConcertListFragment.kt
@@ -1,6 +1,7 @@
 package com.nurisis.concertinfoproject.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -28,6 +29,7 @@ class ConcertListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        Log.d("LOG>>", "${(concertViewModel == null)}")
         viewDataBinding = FragmentConcertListBinding.inflate(inflater, container, false).apply {
             viewmodel = concertViewModel
             lifecycleOwner = this@ConcertListFragment
