cde63f964fd51a95b698f1c7668deb4a4fc826f5
nurisis <isoyeong666@naver.com>
2021-01-10 11:24:00 +0900

    Add static class for just testing

---
+++ app/src/main/java/com/nurisis/concertinfoproject/util/Calculator.kt
@@ -0,0 +1,15 @@
+package com.nurisis.concertinfoproject.util
+
+/**
+ * Just for testing
+ * */
+
+object Calculator {
+    fun add(num1:Number, num2:Number) : Number {
+        return num1 + num2
+    }
+
+    fun subtraction(num1:Number, num2:Number) : Number {
+        return num1 - num2
+    }
+}
---
+++ app/src/test/java/com/nurisis/concertinfoproject/util/CalculatorTest.kt
@@ -0,0 +1,27 @@
+package com.nurisis.concertinfoproject.util
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+
+class CalculatorTest {
+
+    @Before
+    fun setUp() {
+        
+    }
+
+    @After
+    fun finish() {
+
+    }
+
+    @Test
+    fun add() {
+    }
+
+    @Test
+    fun subtraction() {
+    }
+}
